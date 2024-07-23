4dd4090fdea9111841779fff25dc63d6dc723add
other-ones <tuk101234@gmail.com>
2024-07-23 16:44:51 -0400

    regular commit

--- tmp.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Generate Gumbel noise
-num_samples = 10
-gumbel_noise = np.random.gumbel(loc=0, scale=1, size=num_samples)
-print(gumbel_noise)
